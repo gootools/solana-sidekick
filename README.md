effd53e05c93aacd21576948e48b5834d06c81e2
John Rees <john@bitsushi.com>
2021-12-24 20:29:30 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Solana Sidekick
+
+### :warning: This is a pre-release! Much more code, docs and tests in the coming week(s)
+
+Demo: https://gootools.github.com/solana-sidekick
+
+[sidekick.js](https://github.com/gootools/solana-sidekick/blob/main/sidekick.js) is a [ServiceWorker](https://developers.google.com/web/fundamentals/primers/service-workers) that automatically batches individual Solana JSON RPC requests without requiring you to change your existing code.
+
+There are a lot of caveats and will be some [open questions](https://github.com/gootools/solana-sidekick/issues). I can almost guarantee you it's not production-ready yet.
+
+## Goals
+
+- Compatible with any JS frontend framework, or lack thereof
+- Simple as possible to install and update, ideally requiring no or very little existing code changes
+- Provide sensible defaults, offering granular configuration and easy overrides
+## Prior Art
+
+Based off https://github.com/johnrees/rate-limited-fetch-service-worker which is no longer needed because either RPCs aren't incorrectly sending CORS errors when rate limiting anymore, or [web3.js](https://github.com/solana-labs/solana-web3.js) is now retrying on CORS errors (I haven't checked recently). [More info](https://github.com/project-serum/anchor/issues/360#issuecomment-860109385)
---
+++ sidekick.js
@@ -0,0 +1,56 @@
+// start configuration -----------------------------
+// when a request is made, any other requests that arrive
+// within 200ms will be batched with it, requests made after
+// that window will create a new batched request
+const BATCH_WINDOW_SIZE_IN_MS = 200;
+// specify which requests you want to intercept
+// this could based on be the url, body, headers etc...
+const requestMatcher = ({ url }) => new URL(url).host.endsWith("solana.com");
+// end configuration -------------------------------
+self.addEventListener("fetch", (event) => {
+    // only intercept requests that are selected
+    // with the above requestMatcher(request) function
+    if (requestMatcher(event.request)) {
+        event.respondWith(batchedFetch(event.request));
+    }
+});
+/**
+ * accepts a Request, includes it in a batched request
+ * waits for the response, extracts the correct data and
+ * returns it as a Response
+ */
+const batchedFetch = (request) => new Promise((resolve) => {
+    debouncedFetch({ request, resolve });
+});
+const debounceAndBatch = (fn) => {
+    let timeout;
+    let args = [];
+    const process = () => {
+        timeout = null;
+        fn(args);
+        args = [];
+    };
+    return (value) => {
+        args.push(value);
+        if (!timeout)
+            timeout = setTimeout(process, BATCH_WINDOW_SIZE_IN_MS);
+    };
+};
+const debouncedFetch = debounceAndBatch(async (requests) => {
+    // inject all request bodies into first request
+    const allRequestBodies = await Promise.all(requests.map(({ request }) => request.json()));
+    const { request } = requests[0];
+    const response = await fetch(request.url, {
+        body: JSON.stringify(allRequestBodies),
+        headers: request.headers,
+        method: request.method,
+    });
+    const allResults = await response.json();
+    allResults.forEach((result, i) => {
+        requests[i].resolve(new Response(JSON.stringify(result), response));
+    });
+});
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+    console.log("sidekick ready!");
+});
